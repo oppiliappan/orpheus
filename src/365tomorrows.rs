6ecd38a7edde876cca1fcd665c745c45fc68123f
NerdyPepper <akshayoppiliappan@gmail.com>
2018-09-09 12:27:25 +0530

    Better syntax, begin work on story parser

--- src/365tomorrows.rs
+++ src/365tomorrows.rs
@@ -8,9 +8,9 @@ use Story;
 pub fn get_rand_story() -> Story {
     let story_list_url = "https://365tomorrows.com/flashes-of-fiction/"
 
-    let mut response = reqwest::get(&story_list_url).unwrap();
+    let response = reqwest::get(&story_list_url).unwrap();
     let document = Html::parse_document(&response.text().unwrap());
-    let story_selector = Selector::parse(r#"a[class=more-link]"#).unwrap();
+    let story_selector = Selector::parse("a.more-link").unwrap();
 
     let mut url_vec = vec![];
     for link in document.select(&story_selector) {
@@ -28,5 +28,19 @@ pub fn get_rand_story() -> Story {
     let story_document = Html::parse_document(&story_request).unwrap();
 
     // get the story title
-    let title_selector = Selector::parse(r#"h1[class=entry-title]"#).unwrap();
+    let title_selector = Selector::parse("h1.entry-title").unwrap();
+    let title = String::new();
+    for elem in story_document.select(&title_selector) {
+        title.push_str( & mut elem.text().collect::<String>() );
+    }
+
+    // get the story content
+    let story_selector = Selector::parse(r#""#).unwrap();
+
+    // get author
+    let author_selector = Selector::parse("strong").unwrap();
+    let author = String::new();
+    for elem in story_document.select(&author_selector) {
+        author.push_str( & mut elem.text().collect::<String>() );
+    }
 }
