9108573ca3ce985dbfd9f7b45e7108862592e1d6
NerdyPepper <akshayoppiliappan@gmail.com>
2018-09-10 12:02:30 +0530

    Set up basic story rendering

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate rand;
 extern crate sciter;
 
 use sciter::dom::Element;
+use rand::prelude::*;
 
 use std::fmt;
 
@@ -12,15 +13,39 @@ pub mod tomorrows365;
 
 pub struct Story {
     title: String,
-    content: String,
+    content: Vec<String>,
 }
 
 impl fmt::Display for Story {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}\n{}", self.title, self.content)
+        write!(f, "{}\n", self.title);
+        write!(f, "\n")
     }
 } 
 
 fn main() {
-    println!("{}", tomorrows365::get_rand_story());
+    let mut rng = thread_rng();
+    let rand_int: usize = rng.gen_range(0, 2);
+
+    let story = match rand_int {
+        0 => classicshorts::get_rand_story(),
+        _ => tomorrows365::get_rand_story(),
+    };
+
+    let mut frame = sciter::Window::new();
+    frame.load_file("ui_templates/story.html");
+    let root = Element::from_window(frame.get_hwnd()).unwrap();
+
+    if let Some(mut ele) = root.find_first("#title").unwrap() {
+        ele.set_text(&story.title);
+    }
+
+    if let Some(mut ele) = root.find_first("#content").unwrap() {
+        for para in story.content {
+            let p = Element::with_text("p", &para).unwrap();
+            ele.append(&p);
+        }
+    }
+
+    frame.run_app();
 }
